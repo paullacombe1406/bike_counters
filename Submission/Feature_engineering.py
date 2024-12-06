558727dec1d2ae12963b1861cecd7a9f15167924
Paul Lacombe <paul.lacombe@polytechnique.edu>
2024-12-06 22:49:40 +0100

    Final feature

--- Submission/Feature_engineering.py
+++ Submission/Feature_engineering.py
@@ -23,17 +23,12 @@ def get_train_data(path="data/train.parquet"):
 
 
 def _encode_columns(X):
-    counter_counts = (
-        X.groupby("site_id")["counter_id"].nunique().reset_index(name="counter_count")
-    )
-    X = X.merge(counter_counts, on="site_id", how="left")
-
     columns_to_drop = [
-        "counter_name",
+        "counter_id",
         "site_id",
         "site_name",
         "coordinates",
-        "counter_technical_id",
+        "counter_technical_id", "bike_count"
     ]
     X = X.drop(columns=columns_to_drop, axis=1)
     return X
@@ -44,14 +39,47 @@ def _encode_dates(X):
     # Encode the date information from the DateOfDeparture columns
     X.loc[:, "year"] = X["date"].dt.year
     X.loc[:, "month"] = X["date"].dt.month
+    X.loc[:, "week_number"] = X["date"].dt.isocalendar().week
     X.loc[:, "day"] = X["date"].dt.day
     X.loc[:, "weekday"] = X["date"].dt.weekday
-    X.loc[:, "weekend?"] = X["date"].dt.weekday.isin([5, 6]).astype(int)
     X.loc[:, "hour"] = X["date"].dt.hour
-    X.loc[:, "evening?"] = X["hour"].apply(lambda x: 1 if 18 <= x <= 23 else 0)
+    X.loc[:, "dayofyear"] = X["date"].dt.dayofyear 
 
     # Finally we can drop the original columns from the dataframe
-    return X.drop(columns=["date"])
+    return X
+
+def get_time_of_day(X):
+    def get_time(hour):
+        if hour > 3 and hour <= 6:
+            return 1
+        if hour > 6 and hour <= 9:
+            return 2
+        elif hour > 9 and hour <= 12:
+            return 3
+        elif hour > 12 and hour <= 17:
+            return 4
+        elif hour > 17 and hour <= 22:
+            return 5
+        else:
+            return 6
+    X["time_of_day"] = X["hour"].apply(get_time)
+    return X
+
+
+def get_season(X)
+    def season(date):
+        if (date > datetime(2020, 9, 21)) & (date < datetime(2020, 12, 21)):
+            return 1
+        if (date > datetime(2020, 12, 20)) & (date < datetime(2021, 3, 20)):
+            return 2
+        if (date > datetime(2021, 3, 19)) & (date < datetime(2021, 6, 21)):
+            return 3
+        if ((date > datetime(2021, 6, 20)) & (date < datetime(2021, 9, 22))) | (
+            (date > datetime(2020, 6, 19)) & (date < datetime(2020, 9, 22))
+        ):
+            return 4
+    X["season"] = X["date"].apply(season)
+    return X
 
 
 def _merge_external_data(X):
@@ -68,8 +96,8 @@ def _merge_external_data(X):
     X["orig_index"] = np.arange(X.shape[0])
     X = pd.merge_asof(
         X.sort_values("date"),
-        df_ext[["date", "u", "tend", "ww", "rr6", "rr12", "rr24", "etat_sol",
-                "ht_neige", "n", "t", "td", "tend24"]].sort_values("date"),
+        df_ext[["date", "pres", "u", "tend", "ww", "rr6", "rr12", "rr24",
+                "etat_sol", "ht_neige", "n", "t", "td", "tend24"]].sort_values("date"),
         on="date",
     )
     # Sort back to the original order
@@ -188,7 +216,7 @@ def train_test_split_temporal(X, y, delta_threshold="30 days"):
 
     return X_train, y_train, X_valid, y_valid
 
-def _add_arrondissement_with_geopandas(X, shapefile_path):
+def _add_arrondissement_with_geopandas(X, shapefile_path="external_data/arrondissements.shp"):
     arrondissements = gpd.read_file(shapefile_path)
 
     X = X.copy()
@@ -202,6 +230,11 @@ def _add_arrondissement_with_geopandas(X, shapefile_path):
     X["district"] = merged["c_ar"].fillna(21).astype(int)
 
     return X
+
+
+def erase_date(X):
+    X = X.copy()
+    return X.drop("date", axis=1)
 
 
 
